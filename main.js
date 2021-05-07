f444dd73ab8281deeb88ff98580276add73cf436
evg226 <evg226@hotmail.com>
2021-05-07 20:56:55 +0500

    HomeWork 1 - закончен

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-function celsiumFunc() {
+// Домашння работа №1  
+// *************************
+
+// п.1
+function tempFunc() {
     var celsiumTemp = document.getElementById("celsiumId").value;
     var farengateDiv = document.getElementById("farengateId");
-    var farengateTemp = (9 / 5) * celsiumTemp + 32;
+    var farengateTemp = Math.round((9 / 5) * celsiumTemp + 32);
+    console.log("C=" + celsiumTemp + " F=" + farengateTemp);
+    farengateDiv.innerHTML = farengateTemp;
 
-    farengateDiv.innerHTML = "Температрура, F " + farengateTemp;
-    alert("Температрура, F " + farengateTemp);
 }
 
+function tempClear() {
+    var farengateDiv = document.getElementById("farengateId");
+    farengateDiv.innerHTML = '';
+}
+
+// п.2
+function varChange() {
+    var var1 = document.getElementById("var1");
+    var var2 = document.getElementById("var2");
+    var a = +var1.value;
+    var b = +var2.value;
+    console.log("до: a=" + a + " b=" + b);
+    a = a + b - (b = a);
+    console.log("после: a=" + a + " b=" + b);
+    var1.value = a;
+    var2.value = b;
+
+}
+
+// п.3
+function varCopy() {
+    var adminEl = document.getElementById("adminId");
+    var nameEl = document.getElementById("nameId");
+    var admin, name;
+    name = nameEl.value;
+    console.log("name=" + name);
+    admin = name;
+    console.log("admin=" + admin);
+    adminEl.innerHTML = admin;
+}
+
+// п.4
+// Проверка моего ответа
+function checkFunc() {
+    var outpt = document.getElementById("checkId");
+    var result = 1000 + "108";//число преобразуется в строку, а сложение - в конкатенацию
+    console.log(result);
+    outpt.innerHTML = result;
+}
+
+// п.5
+// async и defer тега script изучены. Краткое описание в index.html
