d72926118785ef058e9bd8557df00442ed60f800
evg226 <evg226@hotmail.com>
2021-05-12 13:47:49 +0500

    Сделано задание #3

--- main.js
+++ main.js
@@ -1,4 +1,110 @@
-// Домашння работа №1  
+// Домашння работа №3
+// *************************
+
+// п.1
+// Вывод простых чисел от 0 до 100
+function simpleNumbers() {
+    
+    var resultSumpleDiv = document.getElementById("resultSimpleId");
+    var simpleMax = +document.getElementById("simpleMax").value;
+
+    var result = [];
+    var index = 2;
+    while (index < simpleMax) {
+        if (isSimple(index)){
+            result.push(index);
+        }
+        index++;
+    }
+    console.log(result);
+    resultSumpleDiv.innerHTML = result.join(" ");
+
+    function isSimple(number) {
+        var index = 2;
+        while (index <= (number / 2)) {
+            if ((number % index) == 0) {
+                return false;
+            }
+            index++;
+        }
+        return true;
+    }
+}
+
+// п.2-3
+// Корзина Интернет магазина
+function costCart(){
+     var cart = [
+        { nameItem: "Куртка", price: 130, quantity: 2 },
+        { nameItem: "Брюки", price: 90, quantity: 3 },
+        { nameItem: "Галстук",price: 30,quantity: 2}
+     ]
+    var resultISDiv = document.getElementById("resultIS");
+    resultISDiv.innerHTML= countBasketPrice(cart);
+
+    function countBasketPrice(cart) {
+        // var result=0;
+        // for (var index = 0; index < cart.length; index++){
+        //     console.log(cart[index].price);
+        //     result += cart[index].price;
+        // }
+        // console.log(result);
+        
+        // console.log("_________");
+        // var result=0;
+        // for (var item of cart){
+        //     console.log(item.price);
+        //     result += item.price;
+        // }
+        // console.log(result);
+        
+        // console.log("_________");
+        // var result=0;
+        // for (var index in cart){
+        //     console.log(cart[index].price);
+        //     result += cart[index].price;
+        // }
+        // console.log(result);
+        
+        // console.log("_________");
+        var result = 0;
+        cart.forEach(function (item,index) {
+            console.log((index+1) +"-ая цена: "+item.price+ " или " +cart[index].price)
+            result += item.price;
+        });
+        // console.log(result);
+        return result;
+    }
+}
+
+// п.4
+// Вывод чисел от 0 до 9
+
+function forNotBody(){
+    forId = document.getElementById("forId");
+    var index = 0,result=[];
+    for (;;) {
+        result.push(index++);
+        if (index > 9) {
+            break;
+        }
+    }
+    forId.innerHTML = result.join(" ");
+}
+
+// п.5
+// Нарисовать пирамиду с помощью console.log
+
+function pyramid() {
+    var result = "";
+    for (index = 0; index < 20; index++){
+        console.log(result += "X");
+    }
+}
+
+
+
+// Домашння работа №1
 // *************************
 
 // п.1
